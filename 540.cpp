3b5d72cb8337aec39343bff99c0d187e682a9190
Seojin Kim <rlatjwls7882@gmail.com>
2026-09-13 10:13:20 +0900

    Create 540.cpp

---
+++ 540.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    while(n--) cout << "KNU yaho! Let's dance Para Para!\n";
+}

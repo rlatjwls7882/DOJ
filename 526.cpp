24a4ff4a023794338676e30c57f71412e22952fe
Seojin Kim <rlatjwls7882@gmail.com>
2026-09-22 02:13:26 +0900

    Create 526.cpp

---
+++ 526.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;cin>>n;
+    int r=0;
+    while(n--) {
+        int a;cin>>a;
+        r=max(r,a);
+    }
+    cout<<r;
+}
